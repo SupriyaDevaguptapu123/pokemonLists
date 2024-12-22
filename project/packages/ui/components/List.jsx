2f80eaa9df273b736b7eb892ce91532df68edd35
Supriya Devaguptapu <supriya.devagupta@5datainc.com>
2024-12-22 17:53:59 +0530

    task-5

--- project/packages/ui/components/List.jsx
+++ project/packages/ui/components/List.jsx
@@ -1,12 +1,30 @@
 import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export const List = ({ data }) => {
-
+  const dispatch = useDispatch();
+  const pokemon = useSelector((a) => a.pokemon.list);
+  if (!pokemon || pokemon.length === 0) {
+    return <p>No Pokemon data available</p>
+  }
+  console.log(pokemon, 'pokemon')
+  const handler = (url) => {
+      if (url) {
+        dispatch({ type: 'SET_POKEMON_LIST', payload: pokemon?.filter((a) => a?.url !== url) });
+      }
+    }
+    
   return (
     <>
       <p>List</p>
       <div className={'list'}>
-        {data.map(item => (<div className={'list-item'} key={item.url}>{item.name}
+        {pokemon.map(item => (<div className={'list-item'} key={item.url}>{item.name} 
+          <div>
+          <button onClick={() => handler(item.url)}>
+          remove
+        </button>
+        </div>
         </div>))}
       </div>
     </>
